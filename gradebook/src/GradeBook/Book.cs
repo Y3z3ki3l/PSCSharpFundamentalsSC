4ea4de63b0e4cd12aeb229a1dc59c3636a1748c4
frmugu07 <franz.munoz@ext.arcacontal.com>
2019-08-27 12:43:38 -0500

    Testing Jumping with break and continue

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -13,14 +13,15 @@ namespace GradeBook
 
         public void AddGrade(double grade)
         {
-            if (grade <= 100 && grade >= 0)
+            /* if (grade <= 100 && grade >= 0)
             {
                 grades.Add(grade);   
             }
             else
             {
                 Console.WriteLine($"{grade} is a Invalid value\nWill be erased...");
-            }
+            } */
+            grades.Add(grade);
         }
 
         public Statistics GetStatistics()
@@ -62,6 +63,10 @@ namespace GradeBook
 
             for (int index = 0; index < grades.Count; index++)
             {
+                if (grades[index] <= 0)
+                {
+                    break;
+                }
 
                 result.Low = Math.Min(grades[index], result.Low);
                 result.High = Math.Max(grades[index], result.High);
