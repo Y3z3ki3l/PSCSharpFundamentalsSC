6f4628551ce5be41a776680d7876218c508c47a4
frmugu07 <franz.munoz@ext.arcacontal.com>
2019-08-28 21:57:49 -0500

    Chalenge input from console

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -65,7 +65,7 @@ namespace GradeBook
             {
                 if (grades[index] <= 0)
                 {
-                    break;
+                    continue;
                 }
 
                 result.Low = Math.Min(grades[index], result.Low);
@@ -75,6 +75,25 @@ namespace GradeBook
             }
 
             result.Average /= grades.Count;
+
+            switch (result.Average)
+            {
+                case var d when d > 90:
+                    result.Letter = 'A';
+                    break;
+                case var d when d > 80:
+                    result.Letter = 'B';
+                    break;
+                case var d when d > 70:
+                    result.Letter = 'C';
+                    break;
+                case var d when d > 60:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
